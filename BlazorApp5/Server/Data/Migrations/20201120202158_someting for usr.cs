6ac560cd47dc1bdf9e9969394a5801b30c9cb34c
tesar-tech <33880579+tesar-tech@users.noreply.github.com>
2020-11-20 22:01:39 +0100

    Add project files.

---
+++ BlazorApp5/Server/Data/Migrations/20201120202158_someting for usr.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlazorApp5.Server.Data.Migrations
+{
+    public partial class sometingforusr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SomethingForUser",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SomethingForUser",
+                table: "AspNetUsers");
+        }
+    }
+}
